901828641003f0c1b39b0dad692a844e0808aaeb
Andrew S <samum_94@mail.ru>
2023-02-20 20:46:58 +0300

    Parameterized tests created

--- src/test/java/ModelFreelanceTest.java
+++ src/test/java/ModelFreelanceTest.java
@@ -1,24 +1,18 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class ModelFreelanceTest {
-    @Test
-    public void freelance1() {
+    @ParameterizedTest()
+    @CsvFileSource(files = "src/test/resources/modelfreelance.csv")
+    public void freelance(int expected, int income, int expences, int threshold) {
         ModelFreelance service = new ModelFreelance();
 
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
+        // int expected = 3;
+        int actual = service.calculate(income, expences, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void freelance2() {
-        ModelFreelance service = new ModelFreelance();
-
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
 }
